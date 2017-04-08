182fe4d980c7725e7d6b146d2d4dbf442122a34a
jrue3 <jrue3@W766304>
2017-04-08 13:11:52 -0600

    added yelp api

--- src/Mashy.io/Models/YelpSearch.cs
+++ src/Mashy.io/Models/YelpSearch.cs
@@ -7,5 +7,46 @@ namespace Mashy.io.Models
 {
     public class YelpSearch
     {
+        public class Coordinate
+        {
+            public double latitude { get; set; }
+            public double longitude { get; set; }
+        }
+
+        public class Location
+        {
+            public List<string> address { get; set; }
+            public string city { get; set; }
+            public Coordinate coordinate { get; set; }
+            public string country_code { get; set; }
+            public string cross_streets { get; set; }
+            public List<string> display_address { get; set; }
+            public double geo_accuracy { get; set; }
+            public List<string> neighborhoods { get; set; }
+            public string postal_code { get; set; }
+            public string state_code { get; set; }
+        }
+
+        public class Business
+        {
+            public List<List<string>> categories { get; set; }
+            public string display_phone { get; set; }
+            public string id { get; set; }
+            public string image_url { get; set; }
+            public bool is_claimed { get; set; }
+            public bool is_closed { get; set; }
+            public Location location { get; set; }
+            public string mobile_url { get; set; }
+            public string name { get; set; }
+            public string phone { get; set; }
+            public double rating { get; set; }
+            public string rating_img_url { get; set; }
+            public string rating_img_url_large { get; set; }
+            public string rating_img_url_small { get; set; }
+            public int review_count { get; set; }
+            public string snippet_image_url { get; set; }
+            public string snippet_text { get; set; }
+            public string url { get; set; }
+        }
     }
 }
